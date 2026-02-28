627aa6ae4e6a305ce3ebf37592e34edc94c65c43
Santa Rosa, Paul Justin M. <pauljustin.santarosa@gmail.com>
2026-02-28 16:03:14 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace GradeComputation
     {
         static double sw1, sw2, qz1, qz2, assign, lab, exam;
         static double midtermGrade = 0, finalsGrade = 0;
+
         static void Main(string[] args)
         {
             Console.WriteLine("-------------------------------------------------------------------------------");
@@ -30,13 +31,13 @@ namespace GradeComputation
                     Console.WriteLine("MIDTERM COMPUTATION");
                     subjectName();
                     computeGrade();
-                    calculateGrade();
+                    midtermGrade = calculateGrade();
                     break;
                 case "2":
                     Console.WriteLine("FINALS COMPUTATION");
                     subjectName();
                     computeGrade();
-                    calculateGrade();
+                    finalsGrade = calculateGrade();
                     break;
                 case "3":
                     Console.WriteLine("FINAL-FINAL GRADE");
@@ -50,55 +51,59 @@ namespace GradeComputation
                     break;
             }
         }
-            static void subjectName() {
-                Console.Write("ENTER SUBJECT NAME: ");
-                string name = new string(Console.ReadLine());
-            }
-            static void computeGrade() {
-                 
-                Console.Write("Enter SEATWORK 1 1/10: ");
-                sw1 = double.Parse(Console.ReadLine());
-                Console.Write("Enter SEATWORK 2 1/10: ");
-                sw2 = double.Parse(Console.ReadLine());
-                Console.Write("Enter QUIZ 1 1/10: ");
-                qz1 = double.Parse(Console.ReadLine());
-                Console.Write("Enter QUIZ 2 1/10: ");
-                qz2 = double.Parse(Console.ReadLine());
-                Console.Write("Enter ASSIGNMENT 1/10: ");
-                assign = double.Parse(Console.ReadLine());
-                Console.Write("Enter LABORATORY 1/10: ");
-                lab = double.Parse(Console.ReadLine());
-                Console.Write("Enter EXAM 1/50: ");
-                exam = double.Parse(Console.ReadLine());
-            }
-            static double calculateGrade(double sw1, double sw2, double qz1, double qz2, double assign, double lab, double exam)
-            {
-                // FOR MY EXAMPLE ONLY "HOW I WANT THE GRADE TO WORK": Seatworks (20%), Quizzes (20%), Assignment (10%), Lab (10%), Exam (40%)
 
-                double seatworks = ((sw1 + sw2) / 20.0) * 20;
-                double quizzes = ((qz1 + qz2) / 20.0) * 20;
-                double assignmentScore = (assign / 10.0) * 10;
-                double labScore = (lab / 10.0) * 10;
-                double examScore = (exam / 50.0) * 40;
+        static void subjectName()
+        {
+            Console.Write("ENTER SUBJECT NAME: ");
+            string name = Console.ReadLine();
+        }
 
-                double finalGrade = seatworks + quizzes + assignmentScore + labScore + examScore;
+        static void computeGrade()
+        {
+            Console.Write("Enter SEATWORK 1 1/10: ");
+            sw1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter SEATWORK 2 1/10: ");
+            sw2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter QUIZ 1 1/10: ");
+            qz1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter QUIZ 2 1/10: ");
+            qz2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter ASSIGNMENT 1/10: ");
+            assign = double.Parse(Console.ReadLine());
+            Console.Write("Enter LABORATORY 1/10: ");
+            lab = double.Parse(Console.ReadLine());
+            Console.Write("Enter EXAM 1/50: ");
+            exam = double.Parse(Console.ReadLine());
+        }
 
-                Console.WriteLine("-------------------------------------------------------------------------------");
-                Console.WriteLine("Your computed grade is: " + finalGrade);
-                Console.WriteLine("-------------------------------------------------------------------------------");
+        static double calculateGrade()
+        {
+            // Weighting for my own example only: Seatworks (20%), Quizzes (20%), Assignment (10%), Lab (10%), Exam (40%)
+            double seatworks = ((sw1 + sw2) / 20.0) * 20;
+            double quizzes = ((qz1 + qz2) / 20.0) * 20;
+            double assignmentScore = (assign / 10.0) * 10;
+            double labScore = (lab / 10.0) * 10;
+            double examScore = (exam / 50.0) * 40;
 
-                if (finalGrade >= 90)
-                    Console.WriteLine("Equivalent Grade: 1.0 (Excellent)");
-                else if (finalGrade >= 80)
-                    Console.WriteLine("Equivalent Grade: 2.0 (Good)");
-                else if (finalGrade >= 70)
-                    Console.WriteLine("Equivalent Grade: 3.0 (Fair)");
-                else
-                    Console.WriteLine("Equivalent Grade: 5.0 (Failed)");
+            double finalGrade = seatworks + quizzes + assignmentScore + labScore + examScore;
 
-                return finalGrade;
-            }
-             static void computeFinalFinalGrade()
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine("Your computed grade is: " + finalGrade);
+            Console.WriteLine("-------------------------------------------------------------------------------");
+
+            if (finalGrade >= 90)
+                Console.WriteLine("Equivalent Grade: 1.0 (Excellent)");
+            else if (finalGrade >= 80)
+                Console.WriteLine("Equivalent Grade: 2.0 (Good)");
+            else if (finalGrade >= 70)
+                Console.WriteLine("Equivalent Grade: 3.0 (Fair)");
+            else
+                Console.WriteLine("Equivalent Grade: 5.0 (Failed)");
+
+            return finalGrade;
+        }
+
+        static void computeFinalFinalGrade()
         {
             if (midtermGrade == 0 || finalsGrade == 0)
             {
@@ -112,7 +117,6 @@ namespace GradeComputation
             Console.WriteLine("Your FINAL-FINAL grade is: " + finalFinalGrade);
             Console.WriteLine("-------------------------------------------------------------------------------");
         }
-
-
     }
 }
+
