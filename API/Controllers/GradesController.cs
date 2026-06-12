d1b59eb7672bd38750544635a0de33e565c07bb7
Santa Rosa, Paul Justin M. <pauljustin.santarosa@gmail.com>
2026-06-12 18:42:39 +0800

    API

---
+++ API/Controllers/GradesController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GRADECOMPUTATIONAPI.Controllers
+{
+    [Route("api/Grades")]
+    [ApiController]
+    public class GradesController : ControllerBase
+    {
+        private readonly GradeCalculator _appservice;
+
+        public GradesController()
+        {
+            _appservice = new GradeCalculator();
+        }
+
+        // GET: api/Grades
+        [HttpGet]
+        public ActionResult<IEnumerable<DModels>> GetAllGrades()
+        {
+            var records = _appservice.GetGradeLogs();
+            return Ok(records);
+        }
+
+        // GET: api/Grades/5
+        [HttpGet("{id:int}")]
+        public ActionResult<DModels> GetGradeById(int id)
+        {
+            var record = _appservice.GetGradeById(id);
+
+            if (record == null)
+            {
+                return NotFound();
+            }
+            return Ok(record);
+        }
+
+        // POST: api/Grades
+        [HttpPost]
+        public IActionResult CreateGradeRecord([FromBody] DModels account)
+        {
+            if (account == null)
+            {
+                return BadRequest("Grade data is required.");
+            }
+
+            var newGrade = new DModels
+            {
+                StudentName = account.StudentName,
+                SubjectName = account.SubjectName,
+                Sw1 = account.Sw1,
+                Sw2 = account.Sw2,
+                Qz1 = account.Qz1,
+                Qz2 = account.Qz2,
+                Assign = account.Assign,
+                Lab = account.Lab,
+                Exam = account.Exam,
+                MidtermGrade = account.MidtermGrade,
+                FinalsGrade = account.FinalsGrade
+            };
+
+            var created = _appservice.Register(newGrade);
+
+            if (!created)
+            {
+                return Conflict("Grade record could not be created.");
+            }
+
+            return Ok(new { student = newGrade.StudentName });
+        }
+
+        // PATCH: api/Grades/4
+        [HttpPatch("{id:int}")]
+        public IActionResult UpdateAccount(int id, [FromBody] DModels account)
+        {
+            // Fix: Added the missing condition check here
+            if (account == null)
+            {
+                return BadRequest("Account data is required.");
+            }
+
+            var existingAccount = _appservice.GetGradeById(id);
+
+            if (existingAccount == null)
+            {
+                return NotFound();
+            }
+
+            var updatedAccount = new DModels
+            {
+                S_ID = id,
+                StudentName = account.StudentName ?? existingAccount.StudentName, 
+                SubjectName = account.SubjectName ?? existingAccount.SubjectName,
+                Sw1 = existingAccount.Sw1,
+                Sw2 = existingAccount.Sw2,
+                Qz1 = existingAccount.Qz1,
+                Qz2 = existingAccount.Qz2,
+                Assign = existingAccount.Assign,
+                Lab = existingAccount.Lab,
+                Exam = existingAccount.Exam,
+                MidtermGrade = existingAccount.MidtermGrade,
+                FinalsGrade = existingAccount.FinalsGrade
+            };
+
+            _appservice.UpdateUser(updatedAccount);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Grades/5
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteAccount(int id)
+        {
+            var existingAccount = _appservice.GetGradeById(id);
+
+            if (existingAccount == null)
+            {
+                return NotFound();
+            }
+
+            _appservice.RemoveUser(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API/Models/GradeModelsAndServices.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GRADECOMPUTATIONAPI
+{
+    public class DModels
+    {
+        public int S_ID { get; set; }
+        public string StudentName { get; set; }
+        public string SubjectName { get; set; }
+        public double Sw1 { get; set; }
+        public double Sw2 { get; set; }
+        public double Qz1 { get; set; }
+        public double Qz2 { get; set; }
+        public double Assign { get; set; }
+        public double Lab { get; set; }
+        public double Exam { get; set; }
+        public double MidtermGrade { get; set; }
+        public double FinalsGrade { get; set; }
+    }
+
+    public class GradeCalculator
+    {
+        private static string connString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=StudentGradesDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public List<DModels> GetGradeLogs()
+        {
+            List<DModels> recordsList = new List<DModels>();
+            string selectQuery = "SELECT * FROM GradesPup";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    DModels log = new DModels();
+                    log.S_ID = Convert.ToInt32(reader["S_ID"]);
+                    log.StudentName = reader["StudentName"].ToString();
+                    log.SubjectName = reader["SubjectName"].ToString();
+                    log.Sw1 = Convert.ToDouble(reader["Sw1"]);
+                    log.Sw2 = Convert.ToDouble(reader["Sw2"]);
+                    log.Qz1 = Convert.ToDouble(reader["Qz1"]);
+                    log.Qz2 = Convert.ToDouble(reader["Qz2"]);
+                    log.Assign = Convert.ToDouble(reader["Assign"]);
+                    log.Lab = Convert.ToDouble(reader["Lab"]);
+                    log.Exam = Convert.ToDouble(reader["Exam"]);
+                    log.MidtermGrade = Convert.ToDouble(reader["MidtermGrade"]);
+                    log.FinalsGrade = Convert.ToDouble(reader["FinalsGrade"]);
+                    recordsList.Add(log);
+                }
+            }
+            return recordsList;
+        }
+
+        public DModels GetGradeById(int id)
+        {
+            DModels log = null;
+            string selectQuery = "SELECT * FROM GradesPup WHERE S_ID = @id";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                command.Parameters.AddWithValue("@id", id);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    log = new DModels();
+                    log.S_ID = Convert.ToInt32(reader["S_ID"]);
+                    log.StudentName = reader["StudentName"].ToString();
+                    log.SubjectName = reader["SubjectName"].ToString();
+                    log.Sw1 = Convert.ToDouble(reader["Sw1"]);
+                    log.Sw2 = Convert.ToDouble(reader["Sw2"]);
+                    log.Qz1 = Convert.ToDouble(reader["Qz1"]);
+                    log.Qz2 = Convert.ToDouble(reader["Qz2"]);
+                    log.Assign = Convert.ToDouble(reader["Assign"]);
+                    log.Lab = Convert.ToDouble(reader["Lab"]);
+                    log.Exam = Convert.ToDouble(reader["Exam"]);
+                    log.MidtermGrade = Convert.ToDouble(reader["MidtermGrade"]);
+                    log.FinalsGrade = Convert.ToDouble(reader["FinalsGrade"]);
+                }
+            }
+            return log;
+        }
+
+        public bool Register(DModels newGrade)
+        {
+            string insertQuery = "INSERT INTO GradesPup (StudentName, SubjectName, Sw1, Sw2, Qz1, Qz2, Assign, Lab, Exam, MidtermGrade, FinalsGrade) " +
+                                 "VALUES (@StudentName, @SubjectName, @Sw1, @Sw2, @Qz1, @Qz2, @Assign, @Lab, @Exam, @MidtermGrade, @FinalsGrade)";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand(insertQuery, connection);
+                cmd.Parameters.AddWithValue("@StudentName", newGrade.StudentName);
+                cmd.Parameters.AddWithValue("@SubjectName", newGrade.SubjectName);
+                cmd.Parameters.AddWithValue("@Sw1", newGrade.Sw1);
+                cmd.Parameters.AddWithValue("@Sw2", newGrade.Sw2);
+                cmd.Parameters.AddWithValue("@Qz1", newGrade.Qz1);
+                cmd.Parameters.AddWithValue("@Qz2", newGrade.Qz2);
+                cmd.Parameters.AddWithValue("@Assign", newGrade.Assign);
+                cmd.Parameters.AddWithValue("@Lab", newGrade.Lab);
+                cmd.Parameters.AddWithValue("@Exam", newGrade.Exam);
+                cmd.Parameters.AddWithValue("@MidtermGrade", newGrade.MidtermGrade);
+                cmd.Parameters.AddWithValue("@FinalsGrade", newGrade.FinalsGrade);
+
+                connection.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public bool UpdateUser(DModels updatedAccount)
+        {
+            string updateQuery = "UPDATE GradesPup SET " +
+                                 "StudentName = @StudentName, " +
+                                 "SubjectName = @SubjectName, " +
+                                 "Sw1 = @Sw1, " +
+                                 "Sw2 = @Sw2, " +
+                                 "Qz1 = @Qz1, " +
+                                 "Qz2 = @Qz2, " +
+                                 "Assign = @Assign, " +
+                                 "Lab = @Lab, " +
+                                 "Exam = @Exam, " +
+                                 "MidtermGrade = @MidtermGrade, " +
+                                 "FinalsGrade = @FinalsGrade " +
+                                 "WHERE S_ID = @id";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand(updateQuery, connection);
+                cmd.Parameters.AddWithValue("@id", updatedAccount.S_ID);
+                cmd.Parameters.AddWithValue("@StudentName", updatedAccount.StudentName);
+                cmd.Parameters.AddWithValue("@SubjectName", updatedAccount.SubjectName);
+                cmd.Parameters.AddWithValue("@Sw1", updatedAccount.Sw1);
+                cmd.Parameters.AddWithValue("@Sw2", updatedAccount.Sw2);
+                cmd.Parameters.AddWithValue("@Qz1", updatedAccount.Qz1);
+                cmd.Parameters.AddWithValue("@Qz2", updatedAccount.Qz2);
+                cmd.Parameters.AddWithValue("@Assign", updatedAccount.Assign);
+                cmd.Parameters.AddWithValue("@Lab", updatedAccount.Lab);
+                cmd.Parameters.AddWithValue("@Exam", updatedAccount.Exam);
+                cmd.Parameters.AddWithValue("@MidtermGrade", updatedAccount.MidtermGrade);
+                cmd.Parameters.AddWithValue("@FinalsGrade", updatedAccount.FinalsGrade);
+
+                connection.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public bool RemoveUser(int id)
+        {
+            string deleteQuery = "DELETE FROM GradesPup WHERE S_ID = @id";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand(deleteQuery, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+    }
+}
